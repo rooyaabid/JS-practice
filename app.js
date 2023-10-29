cafcfc5ab172e8c1ce2c44caf752f60dfb1df044
rooya <rooyaabid93@gmail.com>
2023-10-29 14:51:15 -0400

    js practice

---
+++ app.js
@@ -0,0 +1,7 @@
+let cash = 50 
+let price = 40
+let isStoreOpen = true
+
+// 'give receipt' : 'do not give receipt' 
+
+cash >= price && isStoreOpen ?  console.log('give receipt' ) : console.log('do not give receipt'); 
